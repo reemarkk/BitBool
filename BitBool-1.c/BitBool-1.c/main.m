186b1aeb1e8e358240ecff7cbad2758f1ebdcd8e
reemarkk <148678998+reemarkk@users.noreply.github.com>
2024-10-21 20:47:17 +0400

    Update main.m

--- BitBool-1.c/BitBool-1.c/main.m
+++ BitBool-1.c/BitBool-1.c/main.m
@@ -10,19 +10,20 @@ int getbit(BitBool *object , int index ){
         return -1;
     }
     return (object->number >>index) & 1;
+    // >> 'number' right by 'index' to bring the target bit to the rightmost position
+    // & with 1 to isolate that bit and return its value
+    // if bit = 1, 1 & 1 = 1
+    // if bit = 0, 1 & 0 = 0
 }
 
 void setbit(BitBool* object , int index, int newbit){
     if( index < 0 || index >= 8){
         printf("Index out of range.\n");
         return;
-    }
-    if(newbit){
-        object->number |= (1<<index);
-    }
-    else {
-        object -> number &= ~(1<<index);
-    }
+        }
+    newbit ? (object->number |= (1<<index)) : ( object -> number &= ~(1<<index));
+    // If newbit is 1, sets the bit at index to 1 with | bitwise operator.
+    // If newbit is 0, sets the bit at index to 0 with & and ~ bitwise operator.
 }
 
 int main(void){
@@ -37,6 +38,5 @@ int main(void){
     printf("%d\n", getbit(&myobj, 13));
     printf("%d\n", getbit(&myobj, 2));
     printf("%d\n", getbit(&myobj, 7));
-
 return 0;
 }
