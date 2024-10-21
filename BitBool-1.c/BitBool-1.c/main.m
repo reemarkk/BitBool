ba85934a5e9e2ff2b41400bc5e059c8a98be8c96
reemarkk <148678998+reemarkk@users.noreply.github.com>
2024-10-21 22:20:00 +0400

    Update main.m

--- BitBool-1.c/BitBool-1.c/main.m
+++ BitBool-1.c/BitBool-1.c/main.m
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-typedef struct BitBool{
-    char number;
-} BitBool;
+typedef char BitBool;
 
-int getbit(BitBool *object , int index ){
+int getbit(BitBool object , int index ){
     if( index < 0 || index >= 8){
         printf("Index out of range.\n");
         return -1;
     }
-    return (object->number >>index) & 1;
+    return (object>>index) & 1;
     // >> 'number' right by 'index' to bring the target bit to the rightmost position
     // & with 1 to isolate that bit and return its value
     // if bit = 1, 1 & 1 = 1
@@ -21,22 +19,24 @@ void setbit(BitBool* object , int index, int newbit){
         printf("Index out of range.\n");
         return;
         }
-    newbit ? (object->number |= (1<<index)) : ( object -> number &= ~(1<<index));
+    *object= (*object & ~(1<<index)) | (newbit * (1<<index));
+    //1001 2 -> 1 0100 1011 1001  0100 1101
+    
     // If newbit is 1, sets the bit at index to 1 with | bitwise operator.
     // If newbit is 0, sets the bit at index to 0 with & and ~ bitwise operator.
 }
 
 int main(void){
     BitBool myobj;
-    myobj.number = 7;
+    myobj = 7;
     setbit(&myobj, 13, 1);
     setbit(&myobj, 2, 0);
     setbit(&myobj, 1, 1);
     setbit(&myobj, 7, 1);
     
-    printf("%d\n", getbit(&myobj, 1));
-    printf("%d\n", getbit(&myobj, 13));
-    printf("%d\n", getbit(&myobj, 2));
-    printf("%d\n", getbit(&myobj, 7));
+    printf("%d\n", getbit(myobj, 1));
+    printf("%d\n", getbit(myobj, 13));
+    printf("%d\n", getbit(myobj, 2));
+    printf("%d\n", getbit(myobj, 7));
 return 0;
 }
